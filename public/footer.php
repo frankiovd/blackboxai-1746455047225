8a4f183f41364a60ef48579f8e2be0daf000dbe9
frankiovd <150788857+frankiovd@users.noreply.github.com>
2025-05-05 16:53:40 +0200

    Add public/footer.php 1746456819149

---
+++ public/footer.php
@@ -0,0 +1,5 @@
+<footer class="text-center py-4" style="background-color: #212529; color: #FFFFFF; font-family: 'Open Sans', sans-serif;">
+    <div class="container">
+        &copy; <?= date('Y') ?> FitConnect. Todos los derechos reservados.
+    </div>
+</footer>
