baa8b61c00250ebee57df50f755326fa9782ccf0
frankiovd <150788857+frankiovd@users.noreply.github.com>
2025-05-05 17:09:27 +0200

    Add public/footer.php 1746457766858

--- public/footer.php
+++ public/footer.php
@@ -1,5 +1,30 @@
-<footer class="text-center py-4" style="background-color: #212529; color: #FFFFFF; font-family: 'Open Sans', sans-serif;">
+<footer>
     <div class="container">
-        &copy; <?= date('Y') ?> FitConnect. Todos los derechos reservados.
+        <div class="row">
+            <div class="col-md-4 mb-4 mb-md-0">
+                <h5>FitConnect</h5>
+                <p class="text-white-50">Conectando entrenadores y clientes para un estilo de vida más saludable.</p>
+            </div>
+            <div class="col-md-4 mb-4 mb-md-0">
+                <h5>Enlaces Rápidos</h5>
+                <ul class="footer-links">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="testimonials.php">Testimonios</a></li>
+                    <li><a href="bmi_calculator.php">Calculadora IMC</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>Síguenos</h5>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+        <hr class="my-4" style="background-color: rgba(255,255,255,0.1);">
+        <div class="text-center text-white-50">
+            &copy; <?= date('Y') ?> FitConnect. Todos los derechos reservados.
+        </div>
     </div>
 </footer>
